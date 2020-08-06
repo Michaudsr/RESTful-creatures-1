084125647961f549fead0de1a1f1877714d0ed5d
Pete Fitton <petefitton@gmail.com>
2020-08-06 15:57:43 -0700

    Finished PUT & DELETE CodeAlong

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 // create an instance of layouts
 const ejsLayouts = require("express-ejs-layouts");
 const fs = require("fs");
+const methodOverride = require('method-override');
 
 // tell express we are using ejs
 app.set("view engine", "ejs");
@@ -10,6 +11,9 @@ app.set("view engine", "ejs");
 app.use(ejsLayouts);
 // body-parser middleware
 app.use(express.urlencoded({ extended: false }));
+
+// configuring method override to be used in the app
+app.use(methodOverride('_method'));
 
 app.use("/dinosaurs", require("./controllers/dinosaurs"));
 //(linking your / dinosaurs routes to your dinosaurs.js router)
